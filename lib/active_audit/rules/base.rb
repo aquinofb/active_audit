d3ba29cf7a171241abeb511836d5585f1882a84b
aquinofb <aquinofb@gmail.com>
2015-10-25 14:09:11 -0300

    Configuring rules attributes

--- lib/active_audit/rules/base.rb
+++ lib/active_audit/rules/base.rb
@@ -2,21 +2,39 @@ module ActiveAudit
   module Rules
     module Base
 
+      def attributes(*attrs_list)
+        attrs = attrs_list
+      end
+
+      def required_attributes(*attrs_list)
+        required_attrs = attrs_list
+      end
+
       def when_the(attr, opts={}, &handler)
         rules << create_rule_for(attr, opts, &handler)
       end
 
-      def add_hook name, *opts
+      def add_hook(name, *opts)
         hooks << {:"#{name}" => opts}
       end
 
-      def each_hook &block
+      def each_hook(&block)
         hooks.each do |hook|
           yield(hook.keys.first, hook.values.first)
         end
       end
 
       private
+        def required_attrs
+          @required_attrs ||= []
+          @required_attrs
+        end
+
+        def attrs
+          @attrs ||= []
+          @attrs
+        end
+
         def hooks
           @hooks ||= []
           @hooks
